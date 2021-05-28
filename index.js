92b46c167b374672cbcb02e15f6fabdd291cba28
Nathalia Tescarollo <nathtescarollo@gmail.com>
2021-05-27 22:00:41 -0300

    Implementando os métodos de request

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.use(express.json());
 const port = 3000;
 
 const usuarioNathalia = {
-    id: 1,
+    id: 0,
     nome: "nathalia",
     idade: 25
 };
@@ -16,8 +16,44 @@ app.get('/usuarios', (req, res) => {
     res.json(usuarios);
 });
 
+app.post('/usuarios', (req, res) => {
+    const { nome, idade } = req.body;
+    const id = usuarios.length;
+
+    const novoUsuario = {
+        id,
+        nome,
+        idade
+    };
+
+    usuarios.push(novoUsuario);
+    res.status(201).json(novoUsuario);
+});
+
+app.put('/usuarios/:id', (req, res) => {
+    const { id } = req.params;
+    const { nome, idade } = req.body;
+
+    const usuario = usuarios.find(usuario => usuario.id === Number(id)); // sempre usar === se quiser comparar tipo e valor
+    usuario.nome = nome;
+    usuario.idade = idade;
+
+    res.json(usuario);
+});
+
+app.delete('/usuarios/:id', (req, res) => {
+    const { id } = req.params;
+
+    const index = usuarios.findIndex(usuario => usuario.id === Number(id));
+    usuarios.splice(index, 1);
+
+    res.status(204).send();
+})
+
 app.listen(port, () => {
     console.log(`Server is up and running on port ${port}`);
 });
 
 // curl localhost:3000/usuarios
+// curl --request POST --url http://localhost:3000/usuarios  --header 'Content-Type: application/json'  --data '{"nome":"murilo","idade":23}'
+// curl --request GET --url http://localhost:3000/usuarios
